11a1a6ca6cbf6eaf381baf8ac5cea556a787616a
Joe Wilkerson <74438121+jwilkerson94@users.noreply.github.com>
2021-08-13 16:07:04 -0400

    update to components

--- Project1VS/force-app/main/default/aura/dmAlertRatingLow/dmAlertRatingLowController.js
+++ Project1VS/force-app/main/default/aura/dmAlertRatingLow/dmAlertRatingLowController.js
@@ -1,15 +1,44 @@
 ({
-    User: function(component, event, helper) {
+    PlayerUser: function(component, event, helper) {
         console.log("User");
-        let userList = component.get("c.Users");
-        userList.setCallback(this, function(response){
+        let PlayerList = component.get("c.usersPlayer");
+        PlayerList.setCallback(this, function(response){
             if(response.getState() === "SUCCESS"){
-                component.set("v.userList", response.getReturnValue());
+                component.set("v.PlayerList", response.getReturnValue());
+                console.log(response.getReturnValue());
+            }
+          
+        })
+        $A.enqueueAction(PlayerList);
+    },
+     DMUser: function(component, event, helper) {
+        console.log("Userdm");
+        let DMList = component.get("c.UsersDM");
+        DMList.setCallback(this, function(response){
+            if(response.getState() === "SUCCESS"){
+                component.set("v.DMList", response.getReturnValue());
                 console.log(response.getReturnValue());
             }
             
         })
-        $A.enqueueAction(userList);
+        $A.enqueueAction(DMList);
     },
-    
+    userinfo: function(component, event, helper) {
+        console.log("userinfo");
+        let userinfo = component.get("c.UserInfo");
+        userinfo.setCallback(this, function(response){
+            let respMap = response.getReturnValue();
+            if(response.getState() === "SUCCESS"){
+                component.set("v.userinfo", response.getReturnValue());
+            }
+            console.log(respMap.User_Type__c +' here');
+            if(respMap.User_Type__c != 'Player'){
+                component.set("v.ishiddendm", false);
+                component.set("v.isPlayer", false);
+            }
+            
+        })
+        $A.enqueueAction(userinfo);
+    },
+
 })
